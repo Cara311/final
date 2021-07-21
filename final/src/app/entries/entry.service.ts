3db289bb84a604db807067667a8ae0f408a15b1d
Cara <caradevey@yahoo.com>
2021-07-20 20:18:37 -0600

    Another fix.

--- final/src/app/entries/entry.service.ts
+++ final/src/app/entries/entry.service.ts
@@ -14,7 +14,7 @@ export class EntryService {
   private entries: Entry[] = [
     new Entry('5/12/21', 'Ate cheese. Wanted more. Human said, "no."', 'https://images.unsplash.com/photo-1596854650262-2c415cb9f3ee?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80'),
     new Entry('6/20/21', 'Ran until crashed into fence. Fence hurt. No more run.', 'https://images.pexels.com/photos/3487734/pexels-photo-3487734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
-    //new Entry('8/1/21', 'Slept on couch. Couch nice.', 'https://images.pexels.com/photos/731022/pexels-photo-731022.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')
+    new Entry('8/1/21', 'Slept on couch. Couch nice.', 'https://images.pexels.com/photos/731022/pexels-photo-731022.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')
   ];
 
   setEntries(entries: Entry[]) {
